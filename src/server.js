65b83ae4cbac22fe2bd1694c0bc68520c210a0c7
Vladlen8432 <127045075+Vladlen8432@users.noreply.github.com>
2024-09-30 18:49:02 +0300

    updated

--- src/server.js
+++ src/server.js
@@ -3,7 +3,6 @@ import cors from "cors";
 import pino from "pino";
 import expressPinoLogger from "express-pino-logger";
 import contactsRouter from "./routes/contactsRoutes.js";
-// import { getContactById } from "./controllers/contactsController.js";
 
 const logger = pino();
 const expressLogger = expressPinoLogger({ logger });
@@ -30,74 +29,3 @@ function setupServer() {
 }
 
 export { setupServer };
-
-// import express from "express";
-// import cors from "cors";
-// import pino from "pino";
-// import expressPinoLogger from "express-pino-logger";
-// import contactsRouter from "./routes/contactsRoutes.js";
-// import { getContactById } from "./controllers/contactsController.js";
-
-// const logger = pino();
-// const expressLogger = expressPinoLogger({ logger });
-
-// function setupServer() {
-//   const app = express();
-
-//   app.use(cors());
-
-//   app.use(expressLogger);
-
-//   app.use(express.json());
-
-//   app.use("/contacts", contactsRouter);
-
-//   app.get("/contacts/:contactId", getContactById);
-
-//   app.use((res) => {
-//     res.status(404).json({
-//       message: "Not found",
-//     });
-//   });
-
-//   const port = process.env.PORT || 3000;
-//   app.listen(port, () => {
-//     logger.info(`Server is running on port ${port}`);
-//   });
-// }
-
-// export { setupServer };
-
-// import express from "express";
-// import cors from "cors";
-// import pino from "pino";
-// import expressPinoLogger from "express-pino-logger";
-// import contactsRouter from "./routes/contactsRoutes.js";
-
-// const logger = pino();
-// const expressLogger = expressPinoLogger({ logger });
-
-// function setupServer() {
-//   const app = express();
-
-//   app.use(cors());
-
-//   app.use(expressLogger);
-
-//   app.use(express.json());
-
-//   app.use("/contacts", contactsRouter);
-
-//   app.use((res) => {
-//     res.status(404).json({
-//       message: "Not found",
-//     });
-//   });
-
-//   const port = process.env.PORT || 3000;
-//   app.listen(port, () => {
-//     logger.info(`Server is running on port ${port}`);
-//   });
-// }
-
-// export { setupServer };
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -19,15 +19,3 @@ export const findContactById = async (contactId) => {
     throw error;
   }
 };
-
-// import contactsModel from "../models/contacts.js";
-
-// export const getAllContacts = async () => {
-//   const contacts = await contactsModel.find();
-//   return contacts;
-// };
-
-// export const getContactById = async (contactId) => {
-//   const contact = await contactsModel.find(contactId);
-//   return contact;
-// };
