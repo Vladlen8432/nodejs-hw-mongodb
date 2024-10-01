733d89892541f1286b57736f316afef56cff8275
Vladlen8432 <127045075+Vladlen8432@users.noreply.github.com>
2024-10-01 11:33:46 +0300

    update

--- src/controllers/contactsController.js
+++ src/controllers/contactsController.js
@@ -1,5 +1,7 @@
 import { getAllContacts } from "../services/contacts.js";
-import {findContactById} from '../services/contacts.js'
+import { findContactById } from "../services/contacts.js";
+
+import mongoose from "mongoose";
 export const getContactsController = async (req, res) => {
   try {
     const contacts = await getAllContacts();
@@ -22,12 +24,18 @@ export const getContactsController = async (req, res) => {
 export const getContactById = async (req, res) => {
   const { contactId } = req.params;
 
+  if (!mongoose.Types.ObjectId.isValid(contactId)) {
+    return res.status(400).json({
+      message: "Invalid contact ID format",
+    });
+  }
+
   try {
     const contact = await findContactById(contactId);
 
     if (!contact) {
       return res.status(404).json({
-        message: 'Contact not found',
+        message: "Contact not found",
       });
     }
 
@@ -37,9 +45,31 @@ export const getContactById = async (req, res) => {
       data: contact,
     });
   } catch (error) {
-    res.status(500).json({ message: 'Server error', error });
+    res.status(500).json({ message: "Server error", error });
   }
 };
+
+// export const getContactById = async (req, res) => {
+//   const { contactId } = req.params;
+
+//   try {
+//     const contact = await findContactById(contactId);
+
+//     if (!contact) {
+//       return res.status(404).json({
+//         message: "Contact not found",
+//       });
+//     }
+
+//     res.status(200).json({
+//       status: 200,
+//       message: `Successfully found contact with id ${contactId}!`,
+//       data: contact,
+//     });
+//   } catch (error) {
+//     res.status(500).json({ message: "Server error", error });
+//   }
+// };
 
 // import { getAllContacts } from "../services/contacts.js";
 
--- src/routes/contactsRoutes.js
+++ src/routes/contactsRoutes.js
@@ -1,8 +1,18 @@
+// import express from "express";
+// import { getContactsController } from "../controllers/contactsController.js";
+
+// const router = express.Router();
+
+// router.get("/", getContactsController);
+
+// export default router;
+
 import express from 'express';
-import { getContactsController } from '../controllers/contactsController.js';
+import { getContactsController, getContactById } from '../controllers/contactsController.js';
 
 const router = express.Router();
 
 router.get('/', getContactsController);
+router.get('/:contactId', getContactById);
 
 export default router;
