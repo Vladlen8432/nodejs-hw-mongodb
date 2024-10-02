867adc20932bb90506be9131f89b74d451b3d627
Vladlen8432 <127045075+Vladlen8432@users.noreply.github.com>
2024-10-02 16:10:12 +0300

    update

--- src/routes/contactsRoutes.js
+++ src/routes/contactsRoutes.js
@@ -1,18 +1,12 @@
-// import express from "express";
-// import { getContactsController } from "../controllers/contactsController.js";
-
-// const router = express.Router();
-
-// router.get("/", getContactsController);
-
-// export default router;
-
-import express from 'express';
-import { getContactsController, getContactById } from '../controllers/contactsController.js';
+import express from "express";
+import {
+  getContactsController,
+  getContactById,
+} from "../controllers/contactsController.js";
 
 const router = express.Router();
 
-router.get('/', getContactsController);
-router.get('/:contactId', getContactById);
+router.get("/", getContactsController);
+router.get("/:contactId", getContactById);
 
 export default router;
