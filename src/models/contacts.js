cdf100fbc9e42e3042b0044955c32b09e3e25ed3
Vladlen8432 <127045075+Vladlen8432@users.noreply.github.com>
2024-10-15 16:05:40 +0300

    updated

--- src/models/contacts.js
+++ src/models/contacts.js
@@ -11,7 +11,11 @@ const contactSchema = new mongoose.Schema(
       enum: ["work", "home", "personal"],
       required: true,
       default: "personal",
-      // ref: "User",
+    },
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
     },
   },
   {
@@ -25,6 +29,37 @@ const contactSchema = new mongoose.Schema(
   }
 );
 
-const contactsModel = mongoose.model("contact", contactSchema);
+const contactsModel = mongoose.model("Contact", contactSchema);
 
 export default contactsModel;
+
+// import mongoose from "mongoose";
+
+// const contactSchema = new mongoose.Schema(
+//   {
+//     name: { type: String, required: true },
+//     phoneNumber: { type: String, required: true },
+//     email: { type: String, required: false },
+//     isFavourite: { type: Boolean, default: false },
+//     contactType: {
+//       type: String,
+//       enum: ["work", "home", "personal"],
+//       required: true,
+//       default: "personal",
+//       ref: "User",
+//     },
+//   },
+//   {
+//     timestamps: true,
+//     toJSON: {
+//       transform: (doc, ret) => {
+//         delete ret.__v;
+//         return ret;
+//       },
+//     },
+//   }
+// );
+
+// const contactsModel = mongoose.model("contact", contactSchema);
+
+// export default contactsModel;
--- src/services/contacts.js
+++ src/services/contacts.js
@@ -1,18 +1,18 @@
 import contactsModel from "../models/contacts.js";
 
-export const getAllContacts = async () => {
-  const contacts = await contactsModel.find();
+export const getAllContacts = async (userId) => {
+  const contacts = await contactsModel.find({ userId });
   return contacts;
 };
 
-export const getContactById = async (contactId) => {
-  const contact = await contactsModel.findById(contactId);
+export const getContactById = async (contactId, userId) => {
+  const contact = await contactsModel.findOne({ _id: contactId, userId });
   return contact;
 };
 
-export const findContactById = async (contactId) => {
+export const findContactById = async (contactId, userId) => {
   try {
-    const contact = await contactsModel.findById(contactId);
+    const contact = await contactsModel.findOne({ _id: contactId, userId });
     return contact;
   } catch (error) {
     console.error(`Error finding contact by ID: ${error.message}`);
@@ -25,14 +25,53 @@ export const createContact = async (contactData) => {
   return newContact;
 };
 
-export const updateContactById = async (contactId, updateData) => {
-  return contactsModel.findByIdAndUpdate(
-    contactId,
+export const updateContactById = async (contactId, updateData, userId) => {
+  return contactsModel.findOneAndUpdate(
+    { _id: contactId, userId },
     { $set: updateData },
     { new: true, runValidators: true }
   );
 };
 
-export const deleteContactById = async (contactId) => {
-  return contactsModel.findByIdAndDelete(contactId);
+export const deleteContactById = async (contactId, userId) => {
+  return contactsModel.findOneAndDelete({ _id: contactId, userId });
 };
+
+// import contactsModel from "../models/contacts.js";
+
+// export const getAllContacts = async () => {
+//   const contacts = await contactsModel.find();
+//   return contacts;
+// };
+
+// export const getContactById = async (contactId) => {
+//   const contact = await contactsModel.findById(contactId);
+//   return contact;
+// };
+
+// export const findContactById = async (contactId) => {
+//   try {
+//     const contact = await contactsModel.findById(contactId);
+//     return contact;
+//   } catch (error) {
+//     console.error(`Error finding contact by ID: ${error.message}`);
+//     throw error;
+//   }
+// };
+
+// export const createContact = async (contactData) => {
+//   const newContact = await contactsModel.create(contactData);
+//   return newContact;
+// };
+
+// export const updateContactById = async (contactId, updateData) => {
+//   return contactsModel.findByIdAndUpdate(
+//     contactId,
+//     { $set: updateData },
+//     { new: true, runValidators: true }
+//   );
+// };
+
+// export const deleteContactById = async (contactId) => {
+//   return contactsModel.findByIdAndDelete(contactId);
+// };
